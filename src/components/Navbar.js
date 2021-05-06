3086c11c06fc11a4b498c78e456f5968cfdc4fec
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-05-06 19:53:28 +0100

    css changes on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import './styles/Navbar.css';
 
 function Navbar() {
     return (
-        <div id="container">
+        <nav id="container">
             <ul id="nav-ul">
                 <li><a href="./index.js">Home</a></li>
                 <li><a href="#">Projects</a></li>
                 <li><a href="#">Resume</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
-        </div>
+        </nav>
     );
 }
 
