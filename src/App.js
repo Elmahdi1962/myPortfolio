3525dfcffa5af9567183e87fddc841be74520f17
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-05-24 20:40:53 +0100

    sever problems fixed for now and fixed the navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 //import components
 import Navbar from './components/Navbar';
 import Home from './components/Home';
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,18 @@ function Navbar() {
     
     return (
         <header>
-            <h1 id="logo">EL</h1>
-            <nav id="container">
-                <ul id="nav-ul">
-                    <li><a href="#home-section">Home</a></li>
-                    <li><a href="#projects-section">Projects</a></li>
-                    <li><a href="#home-section">Resume</a></li>
-                </ul>      
-  
-            </nav>
+            <div id="nav-container">
+                <h1 id="logo">EL</h1>
+                <nav id="container">
+                    <ul id="nav-ul">
+                        <li><a href="#home-section">Home</a></li>
+                        <li><a href="#projects-section">Projects</a></li>
+                        <li><a href="#home-section">Resume</a></li>
+                    </ul>      
+    
+                </nav>
+            </div>
+            
 
         </header>
         
